a65a1219a8413ee48165bc6f7516a2fe703ca8f2
makarovas <makarofalex@gmail.com>
2024-04-29 20:53:56 +0300

    add memo to 1st version

--- tictaktoe/src/hooks/useTTT.tsx
+++ tictaktoe/src/hooks/useTTT.tsx
@@ -1,55 +1,54 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 const initValue = () => Array(9).fill('');
 
+const WINNING_PATTERNS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 export const useTTT = () => {
   const [board, setBoard] = useState<string[]>(initValue());
-  const [isXNext, setIsXNext] = useState<boolean | null>(null);
-  const WINNING_PATTERNS = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
+  const [isXNext, setIsXNext] = useState(true);
 
-  const calculateWinner = (currBoard: string[]) => {
+  const calculatedWinner = useMemo(() => {
     for (let i = 0; i < WINNING_PATTERNS.length; i++) {
       const [a, b, c] = WINNING_PATTERNS[i];
-
+      console.log(WINNING_PATTERNS[i], 'WINNING_PATTERNS[i];');
+      console.log(board[a], board[b], board[c]);
       if (
-        currBoard[a] &&
-        currBoard[a] === currBoard[b] &&
-        currBoard[a] === currBoard[c]
+        (board[a] === 'X' || board[a] === 'O') &&
+        board[a] === board[b] &&
+        board[a] === board[c]
       ) {
-        return currBoard[a];
+        return board[a];
       }
     }
     return null;
-  };
+  }, [board]);
 
   const handleClick = (index: number) => {
-    
-    const winner = calculateWinner(board);
-    if (winner || board[index]) return;
+    if (calculatedWinner || board[index]) return;
     const newBoard = [...board];
-    newBoard[index] = isXNext ? 'X' : 'O ';
+    newBoard[index] = isXNext ? 'X' : 'O';
     setBoard(newBoard);
     setIsXNext(!isXNext);
   };
 
   const getStatusMessage = () => {
-    const winner = calculateWinner(board);
-    if (winner) return `Player ${winner} wins!`;
+    if (calculatedWinner) return `Player ${calculatedWinner} wins!`;
     if (!board.includes('')) return `Its a draw!`;
-    return `Player${isXNext ? 'X' : 'O'} turn`;
+    return `Player ${isXNext ? 'X' : 'O'} turn`;
   };
 
   const resetGame = () => {
-    setBoard(initValue);
+    setBoard(initValue());
     setIsXNext(true);
   };
 
